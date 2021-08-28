e6b05f32dec7938903c5484b3a2f1fe4860973d2
xiongwei <>
2021-08-28 16:48:17 +0800

    feat：检查用户主目录

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -3,15 +3,22 @@
 const semver = require('semver')
 const colors = require('colors')
 const rootCheck = require('root-check')
+const userHome = require('user-home')
+const pathExists = require('path-exists').sync
 
 const pkg = require('../package.json')
 const logs = require('@panda-cli/logs')
 const constant = require('./const')
 
 function core() {
-  checkVersion()
-  checkNodeVersion()
-  checkRoot()
+  try {
+    checkVersion()
+    checkNodeVersion()
+    checkRoot()
+    checkUserHome()
+  } catch (error) {
+    logs.error(error.message)
+  }
 }
 
 // 打印 package 版本
@@ -25,7 +32,7 @@ function checkNodeVersion() {
   const lowestVersion = constant.LOWEST_NODE_VERSION
   const compareVersionBoolean = semver.gte(currentVersion, lowestVersion)
   if (!compareVersionBoolean) {
-    logs.error(`需要安装${lowestVersion}以上的版本`)
+    throw new Error(colors.red(`需要安装${lowestVersion}以上的版本`))
   }
 }
 
@@ -33,6 +40,13 @@ function checkNodeVersion() {
 function checkRoot() {
   rootCheck()
   // console.log(process.getuid())
+}
+
+// 检查用户主目录
+function checkUserHome() {
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red('当前登录用户主目录不存在！'))
+  }
 }
 
 module.exports = core
